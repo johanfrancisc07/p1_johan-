8fce1f94a4f3850fa04331179c97c70b1d2531bf
johanfrancisc07 <70924226+johanfrancisc07@users.noreply.github.com>
2022-06-06 12:47:11 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using p1_ap1_johan_20150005.UI.Registros;
+using p1_ap1_johan_20150005.UI.Consultas;
+
+namespace p1_ap1_johan_20150005
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrarButton_Click(object sender, RoutedEventArgs e)
+        {
+            R_Aportes R_Aportes = new R_Aportes();
+            R_Aportes.Show();
+        }
+
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            C_Aportes C_Aportes = new C_Aportes();
+            C_Aportes.Show();
+        }
+    }
+}
